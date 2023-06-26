78b9c7c28717a23d1d1eec0e673a886a87d0aab2
benmeisterz <ben.naldoza@gmail.com>
2023-06-26 15:46:54 +0800

    ternary

--- src/App.jsx
+++ src/App.jsx
@@ -61,17 +61,19 @@ function App() {
 function Menu() {
   const pizzas = pizzaData
   // const pizzas = []
+  const numPizzas = pizzas.length
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {pizzas && (
+      {numPizzas > 0 ? (
         <ul className='pizzas'>
           {pizzas.map(pizza => (
             <Pizza pizzaobj={pizza} key={pizza.name} />
           ))}
         </ul>
-      )}
+      )
+        : <p>We are still working on our menu. Please come back later</p>}
     </main>
   )
 }
@@ -101,7 +103,7 @@ function Header() {
 
 function Footer() {
   const hour = new Date().getHours()
-  const openHour = 12
+  const openHour = 9
   const closeHour = 22
   const isOpen = hour >= openHour && hour <= closeHour
 
@@ -119,12 +121,13 @@ function Footer() {
 
 
   return (<footer className='footer'>
-    {isOpen && (
+    {isOpen ? (
       <div className="order">
-        <p>We're open until {closeHour}:00. Come visit us or order online</p>
+        <p>We're open until {closeHour}:00. Come visit us or order online.</p>
         <button className="btn">Order</button>
       </div>
-    )}
+    ) : <p>We're happy to serve you between {openHour}:00 and {closeHour}:00.</p>
+    }
   </footer>)
 }
 export default App
